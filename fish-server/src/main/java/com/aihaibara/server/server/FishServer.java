6e9f4374bbc3aae8ccf0d85e846060c4072707a8
曹良强 <2990807723@qq.com>
2021-06-15 09:45:08 +0800

    服务端server添加

--- fish-server/src/main/java/com/aihaibara/server/server/FishServer.java
+++ fish-server/src/main/java/com/aihaibara/server/server/FishServer.java
@@ -2,16 +2,21 @@ package com.aihaibara.server.server;
 
 
 import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.PooledByteBufAllocator;
+import io.netty.channel.ChannelOption;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.net.InetSocketAddress;
+
 @Data
 @Slf4j
-@Service("ChatServer")
+@Service("FishServer")
 public class FishServer {
 
     // 服务器端口
@@ -31,6 +36,17 @@ public class FishServer {
 
     //启动
     public void run(){
+        //1 reactor 线程
+        b.group(bg,wg);
+        //2 nio类型的channel
+        b.channel(NioServerSocketChannel.class);
+        // 监听端口
+        b.localAddress(new InetSocketAddress(port));
+
+        //4 通道选项
+        b.option(ChannelOption.SO_KEEPALIVE, true);
+        b.option(ChannelOption.ALLOCATOR,
+                PooledByteBufAllocator.DEFAULT);
 
     }
 }
